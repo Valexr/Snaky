11655ce383d6c7e978683dc9ffd2feb99a5b4139
Valexr <valexr@gmail.com>
2023-02-05 13:44:49 +0300

    fix stop button

--- src/components/Score.svelte
+++ src/components/Score.svelte
@@ -5,9 +5,10 @@
 <header id="scoreboard">
     <h3>Score: {$score}</h3>
     <h1>
-        Snaky
         {#if $isPlaying}
-            <button id="stop" on:click={stop}>⏹</button>
+            <button id="stop" on:click={stop}>Stop</button>
+        {:else}
+            Snaky
         {/if}
     </h1>
     <h3>Speed: {$speed}</h3>
@@ -22,6 +23,9 @@
         background-color: var(--dark);
         color: var(--primary);
         border: 0;
+        font-family: inherit;
+        font-size: 1.5rem;
+        padding: 0.15rem 1rem;
     }
     #stop:active {
         top: 2px;
