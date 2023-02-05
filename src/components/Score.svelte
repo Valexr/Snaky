d78aebf9ea3b422df82d5dea35b3ca4d5f005abc
Valexr <valexr@gmail.com>
2023-02-05 22:56:34 +0300

    css

--- src/components/Score.svelte
+++ src/components/Score.svelte
@@ -13,23 +13,3 @@
     </h1>
     <h3>Speed: {$speed}</h3>
 </header>
-
-<style>
-    #stop {
-        cursor: pointer;
-        position: relative;
-        vertical-align: middle;
-        box-shadow: var(--shadow);
-        background-color: var(--dark);
-        color: var(--primary);
-        border: 0;
-        font-family: inherit;
-        font-size: 1.5rem;
-        padding: 0.15rem 1rem;
-    }
-    #stop:active {
-        top: 2px;
-        left: 2px;
-        box-shadow: none;
-    }
-</style>
